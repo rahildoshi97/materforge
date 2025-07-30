fe9866001909711e5e8dfaa6c2a6476ae419105b
Rahil Doshi <rahil.doshi@fau.de>
2025-07-30 17:59:06 +0200

    Update sync-repos.sh

--- sync-repos.sh
+++ sync-repos.sh
@@ -3,8 +3,8 @@ echo "Fetching from both remotes..."
 git fetch github
 git fetch origin
 
-echo "Syncing GitHub main -> GitLab master..."
-git push origin github/main:master
+echo "Syncing GitHub main -> GitLab pwlf..."
+git push origin github/main:pwlf
 
 echo "Syncing GitLab master -> GitHub main..."
 git push github origin/master:main
