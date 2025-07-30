9d84e5a40ca80d2d31872670806f668efc3fca57
Rahil Doshi <rahil.doshi@fau.de>
2025-07-30 16:10:58 +0200

    Add repository synchronization script

---
+++ sync-repos.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Fetching from both remotes..."
+git fetch github
+git fetch origin
+
+echo "Syncing GitHub main -> GitLab pwlf..."
+git push origin github/main:pwlf
+
+echo "Syncing GitHub main -> GitLab master..."
+git push origin github/main:master
+
+echo "Syncing GitLab master -> GitHub main..."
+git push github origin/master:main
+
+echo "Sync complete!"
